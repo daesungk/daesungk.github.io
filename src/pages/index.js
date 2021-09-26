ebb5bf0ad60f2de9a9f4341e7db56054e34ea92c
daesungk <daesungk@illinois.edu>
2021-09-25 21:48:54 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
             <p>
 				I'm a J. L. Doob research assistant professor (postdoc) in the <a href="https://math.illinois.edu/" target="_blank" rel="noreferrer">Dept. of Mathematics</a> at the <a href="https://illinois.edu/" target="_blank" rel="noreferrer">University of Illinois at Urbana-Champaign</a>. 
                 I obtained my Ph.D degree in Mathematics at <a href="https://www.purdue.edu/" target="_blank" rel="noreferrer">Purdue University</a> in August 2019 under the supervision of <a href="https://www.math.purdue.edu/people/bio?user=banuelos&amp;page=" target="_blank" rel="noreferrer">Rodrigo Bañuelos</a>.  
-                My Curriculum Vitae can be found <a href="http://daesungk.github.io/resources/cv-daesungk.pdf" target="_blank" rel="noreferrer">here</a>.
+                My Curriculum Vitae can be found <a href="http://daesungk.github.io/resources/cv-dkim.pdf" target="_blank" rel="noreferrer">here</a>.
             </p>
         </Section>
         <Section title="Contact Information">
