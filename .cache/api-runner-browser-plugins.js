2bdf49ea529c103f66df2e311abab206dea6f4d5
Daesung Kim <lui8190@gmail.com>
2022-11-11 10:21:45 -0500

    update

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-123277960-1","head":true,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0,"enableWebVitalsTracking":false},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
